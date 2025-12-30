b9052e81f386c28b7969a9f5d5edfe3a138e28cf
tatancarduar <tatancarduar@hotmail.com>
2025-12-30 11:01:08 -0500

    Components Patterns - Sin Refactorizar

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, NavLink, Navigate} from "react-router-dom";
+import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 import logo from "../logo.svg";
 
 export const Navigation = () => {
@@ -10,25 +10,40 @@ export const Navigation = () => {
 					<img src={logo} alt="React logo" />
 					<ul>
 						<li>
-							<NavLink to="/home" className={({ isActive }) => (isActive ? "nav-active" : "")}>Home</NavLink>
+							<NavLink
+								to="/home"
+								className={({ isActive }) => (isActive ? "nav-active" : "")}
+							>
+								Home
+							</NavLink>
 						</li>
 						<li>
-							<NavLink to="/about" className={({ isActive }) => (isActive ? "nav-active" : "")}>About</NavLink>
+							<NavLink
+								to="/about"
+								className={({ isActive }) => (isActive ? "nav-active" : "")}
+							>
+								About
+							</NavLink>
 						</li>
 						<li>
-							<NavLink to="/users" className={({ isActive }) => (isActive ? "nav-active" : "")}>User</NavLink>
+							<NavLink
+								to="/users"
+								className={({ isActive }) => (isActive ? "nav-active" : "")}
+							>
+								User
+							</NavLink>
 						</li>
 					</ul>
 				</nav>
 
-        <Routes>
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/users" element={<h1>User</h1>} />
-          <Route path="/home" element={<h1>Home</h1>} />
-          <Route path="/*" element={<Navigate to="/home" replace/>} /> {/**replace no me deja regresar al anterior*/}
-        </Routes>
+				<Routes>
+					<Route path="/about" element={<h1>About</h1>} />
+					<Route path="/users" element={<h1>User</h1>} />
+					<Route path="/home" element={<h1>Home</h1>} />
+					<Route path="/*" element={<Navigate to="/home" replace />} />{" "}
+					{/**replace no me deja regresar al anterior*/}
+				</Routes>
 			</div>
-
 		</BrowserRouter>
 	);
 };
