e7444074754f30189c7ae7572ebcc80f978c763f
tatancarduar <tatancarduar@hotmail.com>
2026-01-08 15:14:15 -0500

    Control Props Alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -12,22 +12,13 @@ export const useProduct = ({
 	product,
 	value = 0,
 }: useProductArgs) => {
-
 	const [counter, setCounter] = useState(value);
+	const increaseBy = (value?: number) => {
+		const amount = value ?? 1;
+		const newValue = Math.max(counter + amount, 0);
+		setCounter(newValue);
 
-	const isControlled = useRef(!!onChange); // Con la doble negación convertimos a booleano
-	// Para obtener el valor por de la referencia se usa isControlled.current
-
-	const increaseBy = (value: number = 1) => {
-
-		if (isControlled.current) {
-			return onChange!({ count: value, product });
-		}
-
-		const newCount = Math.max(counter + value, 0);
-		setCounter((prev) => Math.max(prev + value, 0));
-
-		onChange && onChange({ count: newCount, product });
+		onChange && onChange({ count: newValue, product });
 	};
 
 	useEffect(() => {
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -2,43 +2,28 @@ import { useState } from "react";
 import { onChangeArgs, ProductsInCart } from "../interfaces/interfaces";
 
 export const useShoppingCart = () => {
-  
 	const [shoppingCart, setShoppingCart] = useState<{
 		[key: string]: ProductsInCart;
 	}>({});
 
 	const onProductCountChange = ({ count, product }: onChangeArgs) => {
+    console.log(count);
 		setShoppingCart((oldShoppingCart) => {
-			console.log(count);
-			// Verificar si tengo un producto
-			const productInCart: ProductsInCart = oldShoppingCart[product.id] || {
-				...product,
-				count: 0,
-			};
-
-			if (Math.max(productInCart.count + count, 0) > 0) {
-				const updatedProduct: ProductsInCart = {
-					...productInCart,
-					count: productInCart.count + count,
-				};
-				return {
-					...oldShoppingCart,
-					[product.id]: updatedProduct,
-				};
+			if (count === 0) {
+				const { [product.id]: toRemove, ...rest } = oldShoppingCart;
+				return rest;
 			}
-
-			// Borrar el producto del carrito
-			const { [product.id]: toDelete, ...rest } = oldShoppingCart;
 			return {
-				...rest,
+				...oldShoppingCart,
+				[product.id]: { ...product, count },
 			};
 		});
 	};
 
 	return {
-    // Props
-    shoppingCart,
-    // Methods
-    onProductCountChange,
-  };
+		// Props
+		shoppingCart,
+		// Methods
+		onProductCountChange,
+	};
 };
